c497505305437a7e73e36ffe1cf42b95fad5eb90
Garrett <garrettcoleallen@gmail.com>
2022-09-01 22:11:34 -0400

    high and low scores

--- scoreboard.js
+++ scoreboard.js
@@ -7,22 +7,40 @@ async function getGameOptions() {
 }
 
 async function insertNewScore(game, name, score, metaData, validation = "") {
-    if (validateScore(game, name, score, metaData, validation)) {
-        return await Score.create({
-            game,
-            name,
-            score,
-            metaData,
-            date: Date.now()
-        });
-    } else {
-        console.log(`Score submission for ${game} for name ${name} failed to validate and was rejected`);
-    }
-    return false;    
+    return await new Promise((resolve, reject) => {
+        if (validateScore(game, name, score, metaData, validation)) {
+            let scoreObj = Score.create({
+                game,
+                name,
+                score,
+                metaData,
+                date: Date.now()
+            });
+            let higherLower = getScoresHigherAndLowerThan(game, score);
+            Promise.all([scoreObj, higherLower]).then((results) => {
+                return resolve({
+                    score: results[0],
+                    ...results[1]
+                });
+            });
+        } else {
+            console.log(`Score submission for ${game} for name ${name} failed to validate and was rejected`);
+            return resolve(false); 
+        }        
+    });
 }
 
 async function getGame(game, limit, ascending, page) {
     return await Score.findByGame(game, limit, ascending, page);
+}
+
+async function getScoresHigherAndLowerThan(game, score){
+    let scoresGreater = await Score.find({game, score: {"$gt": score}}).countDocuments();
+    let scoresLesser = await Score.find({game, score: {"$lt": score}}).countDocuments();
+    return {
+        scoresGreater,
+        scoresLesser
+    };
 }
 
 module.exports = {
--- web.js
+++ web.js
@@ -32,11 +32,11 @@ function registerScoreEndpoints() {
     res.send(await getGame(game, limit, asc == "true", page));
   })
 
-  app.post('/games/submit', (req, res) => {
+  app.post('/games/submit', async (req, res) => {
     if (req.body.name.length > 100) {
       req.body.name = req.body.name.substr(0, 100);
     }
-    res.send(insertNewScore(req.body.game, req.body.name, req.body.score, req.body.metaData, req.body.validation));
+    res.send(await insertNewScore(req.body.game, req.body.name, req.body.score, req.body.metaData, req.body.validation));
   });
 }
 

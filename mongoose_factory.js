c63ad4022071d8eb2ddb5325675d68cf35c55744
fenix-hub <n.santilio97@gmail.com>
2022-09-10 19:39:20 +0200

    add MONG_DB env variable

--- mongoose_factory.js
+++ mongoose_factory.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/`, 
+    `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`, 
     {useNewUrlParser: true, useUnifiedTopology: true}
 );
 
